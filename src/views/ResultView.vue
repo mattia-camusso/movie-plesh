35da554e89492777de20782f4b7452070d0d3b23
mattia.camusso@gmail.com <mattia.camusso@gmail.com>
2024-07-02 10:10:32 +0200

    3rd

--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -4,16 +4,6 @@
 
 <script lang="js" setup>
 import TestComp from '@/components/TestComp.vue'
-import { useMainStore } from '@/stores/reccomandation'
-import { useRouter } from 'vue-router'
-
-const store = useMainStore()
-const router = useRouter()
-
-const restart = () => {
-  store.reset()
-  router.push('/feed')
-}
 </script>
 
 <style></style>
